e16ddbb2d199214c959b1692cd5b0456f34990cf
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2022-09-21 12:06:42 +0900

    追記

--- drill/chap3-05.sql
+++ drill/chap3-05.sql
@@ -85,3 +85,17 @@ AND c1.PrefecturalID = c2.PrefecturalID
 AND c1.CustomerClassID = c2.CustomerClassID
 AND (c1.PrefecturalID <> 13 OR c2.PrefecturalID <> 13)
 ;
+
+SELECT
+  c1.CustomerName AS "顧客名1",
+  c2.CustomerName AS "顧客名2"
+FROM
+  Customers AS c1
+JOIN
+  Customers AS c2
+  ON c1.CustomerID < c2.CustomerID
+      AND c1.PrefecturalID = c2.PrefecturalID
+      AND c1.CustomerClassID = c2.CustomerClassID
+WHERE
+  c1.PrefecturalID <> 13 OR c2.PrefecturalID <> 13
+;
