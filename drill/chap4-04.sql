041f4eabbee1b380e2a1dab6f72cb3485e79594e
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2022-09-27 09:48:34 +0900

    chap4-04済

--- drill/chap4-04.sql
+++ drill/chap4-04.sql
@@ -57,6 +57,8 @@ SELECT * FROM Sales WHERE CustomerID = 10 AND ProductID = 5 AND SaleDate >= '200
 
 UPDATE
   Sales
+SET
+  Quantity = Quantity + 10
 WHERE
   CustomerID = 10 AND ProductID = 5 AND SaleDate >= '2007-05-31'
 ;
@@ -64,3 +66,17 @@ WHERE
 
 -- 第5問
 
+SELECT * FROM Products WHERE CategoryID = 7;
+
+UPDATE
+  Products
+SET
+  Price =
+    CASE
+      WHEN Price >= 2000 THEN Price * 0.8
+      WHEN Price >= 1000 THEN Price * 0.9
+      ELSE Price
+    END
+WHERE
+  CategoryID = 7
+;
