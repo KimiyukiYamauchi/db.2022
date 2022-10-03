8da03b79af1594e0b0dbadfbc34b1ee08cd1fb56
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2022-10-03 09:10:29 +0900

    chap4-07済

---
+++ drill/chap4-07.sql
@@ -0,0 +1,56 @@
+-- 書いてみよう
+
+SELECT * FROM Salary;
+
+DELETE
+FROM
+  Salary
+;
+
+-- 練習問題
+-- 第1問
+
+SELECT * FROM BelongTo;
+
+DELETE
+FROM
+  BelongTo
+;
+
+-- 第2問
+
+SELECT * FROM Customers;
+
+DELETE
+FROM
+  Customers
+;
+
+
+-- 第3問
+
+SELECT * FROM Sales;
+
+DELETE
+FROM
+  Sales
+;
+
+
+-- 第4問
+
+SELECT * FROM Products;
+
+DELETE
+FROM
+  Products
+;
+
+-- 第5問
+
+SELECT * FROM Employees;
+
+DELETE
+FROM
+  Employees
+;
