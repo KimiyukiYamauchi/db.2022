58cd1011845c4b39199b9b1150b1079bbb625dba
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2024-03-27 09:47:47 +0900

    修正

--- drill/chap3-04.sql
+++ drill/chap3-04.sql
@@ -25,6 +25,39 @@ GROUP BY
 -- 書いてみよう
 
 SELECT
+  ProductName, 
+  AVG(Price *
+    CASE
+      WHEN Quantity IS NULL THEN 0
+      ELSE Quantity
+    END
+    ) AS 平均販売価格
+FROM
+  Products AS p
+LEFT OUTER JOIN
+  Sales AS s
+ON p.ProductID = s.ProductID
+GROUP BY
+  s.ProductID, ProductName
+ORDER BY
+  s.ProductID
+;
+
+SELECT
+  ProductName, 
+  AVG(Price * ifnull(Quantity, 0)) AS 平均販売価格
+FROM
+  Products AS p
+LEFT OUTER JOIN
+  Sales AS s
+ON p.ProductID = s.ProductID
+GROUP BY
+  s.ProductID, ProductName
+ORDER BY
+  s.ProductID
+;
+
+SELECT
   P.ProductID, P.ProductName, 
   AVG(
     P.Price * S.Quantity
@@ -137,6 +170,18 @@ GROUP BY
   A.EmployeeID
 ;
 
+SELECT
+  A.EmployeeID,
+  MAX(EmployeeName) AS "社員名",
+  COUNT(Quantity) AS "販売件数"
+FROM
+  Employees AS A
+    LEFT OUTER JOIN
+  Sales AS B
+    ON A.EmployeeID = B.EmployeeID
+GROUP BY
+  A.EmployeeID
+;
 
 -- 第3問
 
